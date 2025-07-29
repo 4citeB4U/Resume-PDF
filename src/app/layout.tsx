b35f71dad1fad99647460271082835627c4ef115
L. Lee <osirussees@gmail.com>
2025-07-29 03:10:32 +0000

    OK reduce the size. Of this just a little bit. To the officer because us

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="!scroll-smooth">
+    <html lang="en" className="!scroll-smooth" style={{ fontSize: '90%' }}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
